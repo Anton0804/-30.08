8782cb875b9e085058a9c8bcde10f50209122b63
John Doe <johndoe@example.com>
2022-09-13 21:21:55 +0300

    в 2х экземплярах

--- Task33/Program.cs
+++ Task33/Program.cs
@@ -50,3 +50,41 @@ bool result = ArraySearch(arr, search);
 Console.WriteLine($"{result}");
 
 
+// Console.Clear();
+// Console.WriteLine();
+
+// int[] CreateArrayRndInt(int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     Random rnd = new Random();
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = rnd.Next(min, max + 1);
+//     }
+//     return array;
+// }
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+//         else Console.Write($"{array[i]}");
+//     }
+//     Console.Write("]");
+// }
+// int[] Inverse(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = -array[i];
+//     }
+//     return array;
+// }
+// int[] arr = CreateArrayRndInt(10, -100, 100);
+// PrintArray(arr);
+// Console.WriteLine();
+// int[] result = Inverse(arr);
+// PrintArray(result);
+// Console.WriteLine();
+
