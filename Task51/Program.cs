a4d6335352d203ad51e88db55081cf7e667422d1
John Doe <johndoe@example.com>
2022-09-20 22:10:26 +0300

    готово

--- Task51/Program.cs
+++ Task51/Program.cs
@@ -36,3 +36,38 @@ PrintMatrix(array2D);
 int sumResult = SumMidleMatrix(array2D);
 Console.WriteLine();
 Console.WriteLine($"Сумма элементов, находящихся на главной диагонали = {sumResult}");
+
+
+
+// int DiagonalSumMatrix(int[,] matrix)                         Короткое решение
+// {
+//     int diagnalSumMatrix = default;
+//     int oversize = 0;
+//     if (matrix.GetLength(0) > matrix.GetLength(1)) oversize = 1;
+//     for (int i = 0; i < matrix.GetLength(oversize); i++)
+//     {
+
+//         diagnalSumMatrix += matrix[i, i];
+
+//     }
+//     return diagnalSumMatrix;
+// }
+
+// bool restart = true;
+// while (restart)
+// {
+//     Console.Clear();
+
+//     Random randomizer = new Random();
+//     int randomRows = randomizer.Next(2, 6),
+//         randomColumns = randomizer.Next(2, 6);
+
+//     int[,] array2D = CreateMatrixRndInt(randomRows, randomColumns);
+//     PrintMatrix(array2D);
+//     Console.WriteLine("-------------------------------------------------------------");
+//     int result = DiagonalSumMatrix(array2D);
+//     Console.WriteLine($"Сумма элементов главной диагонали = {result}");
+
+//     Console.Write("Ещё раз? \n'Enter' - Да. Что-то другое - Выход.");
+//     restart = Console.ReadKey().Key == ConsoleKey.Enter;
+// }
